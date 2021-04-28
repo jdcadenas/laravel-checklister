7f67cd57f815712077f65049ce36222c806c7181
Jose Cadenas <jdcadenas@gmail.com>
2021-04-27 23:15:59 -0400

    end part two

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Middleware\IsAdmin;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +23,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware'=>'auth'],function() {
+    Route::group(['prefix'=>'admin','as'=>'admin.','middleware'=>'is_admin'],function() {
+      Route::resource( 'pages',App\Http\Controllers\Admin\PageController::class);
+    });
+});
